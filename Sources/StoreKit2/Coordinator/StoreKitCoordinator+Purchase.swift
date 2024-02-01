3927696a081bd900da08d02c0feda2e7dcf40a26
Texas <a.kanarskyi@netpeak.net>
2024-02-01 10:27:54 +0100

    feat: added storekit coordinator

---
+++ Sources/StoreKit2/Coordinator/StoreKitCoordinator+Purchase.swift
@@ -0,0 +1,45 @@
+import Foundation
+import StoreKit
+
+extension StoreKitCoordinator {
+    func purchase(_ product: Product) async throws -> Transaction? {
+        debugPrint("\(StoreKitCoordinator.identifier) purchase \(DebuggingIdentifiers.actionOrEventInProgress) Purchasing product \(product.displayName)... \(DebuggingIdentifiers.actionOrEventInProgress)")
+        // Begin purchasing the `Product` the user selects.
+        let result = try await product.purchase()
+
+        switch result {
+        case .success(let verification):
+            debugPrint("\(StoreKitCoordinator.identifier) purchase \(DebuggingIdentifiers.actionOrEventSucceded) Product Purchased.")
+            debugPrint("\(StoreKitCoordinator.identifier) purchase \(DebuggingIdentifiers.actionOrEventInProgress) Verifying... \(DebuggingIdentifiers.actionOrEventInProgress)")
+            // Check whether the transaction is verified. If it isn't,
+            // this function rethrows the verification error.
+            let transaction = try checkVerified(verification)
+            debugPrint("\(StoreKitCoordinator.identifier) purchase \(DebuggingIdentifiers.actionOrEventSucceded) Verified.")
+            // The transaction is verified. Deliver content to the user.
+            debugPrint("\(StoreKitCoordinator.identifier) purchase \(DebuggingIdentifiers.actionOrEventInProgress) Updating Product status... \(DebuggingIdentifiers.actionOrEventInProgress)")
+            await updateCustomerProductStatus()
+            debugPrint("\(StoreKitCoordinator.identifier) purchase \(DebuggingIdentifiers.actionOrEventSucceded) Updated product status.")
+            // Always finish a transaction - This removes transactions from the queue and it tells Apple that the customer has recieved their items or service.
+            await transaction.finish()
+            debugPrint("\(StoreKitCoordinator.identifier) purchase \(DebuggingIdentifiers.actionOrEventSucceded) Finished transaction.")
+            return transaction
+        case .pending:
+            debugPrint("\(StoreKitCoordinator.identifier) purchase \(DebuggingIdentifiers.actionOrEventFailed) Failed as the transaction is pending.")
+            return nil
+        case .userCancelled:
+            debugPrint("\(StoreKitCoordinator.identifier) purchase \(DebuggingIdentifiers.actionOrEventFailed) Failed as the user cancelled the purchase.")
+            return nil
+        default:
+            debugPrint("\(StoreKitCoordinator.identifier) purchase \(DebuggingIdentifiers.actionOrEventFailed) Failed with result \(result).")
+            return nil
+        }
+    }
+    
+    //This call displays a system prompt that asks users to authenticate with their App Store credentials.
+    //Call this function only in response to an explicit user action, such as tapping a button.
+    func restore() {
+        async {
+            try? await AppStore.sync()
+        }
+    }
+}
---
+++ Sources/StoreKit2/OfferingKeys.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+enum OfferingKeys: String {
+    case sampleNonConsumable = "Sample Non-Consumable"
+    case sampleConsumable = "Sample Consumable"
+    case sampleNonRenewingSubscription = "Sample Non-Renewing Subscription"
+    case sampleAutoRenewableSubscription = "Sample Auto Renewable Subscription"
+    case sampleTierTwoAutoRenewableSubscription = "Sample Tier 2 Auto Renewable Subscription"
+}
