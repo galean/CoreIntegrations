8791970f13977b0b90c41030e24443d920ef6ab2
Texas <a.kanarskyi@netpeak.net>
2024-04-29 11:24:18 +0200

    fix: PromoOffer init

--- Sources/CoreIntegrations/Protocols/PublicProtocols/PromoOffer.swift
+++ Sources/CoreIntegrations/Protocols/PublicProtocols/PromoOffer.swift
@@ -13,4 +13,12 @@ public struct PromoOffer {
     let nonce: UUID
     let signature: Data
     let timestamp: Int
+    
+    public init(offerID: String, keyID: String, nonce: UUID, signature: Data, timestamp: Int) {
+        self.offerID = offerID
+        self.keyID = keyID
+        self.nonce = nonce
+        self.signature = signature
+        self.timestamp = timestamp
+    }
 }
