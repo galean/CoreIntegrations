66de24be6341c2becf864a4aad67e37136fe740f
Texas <a.kanarskyi@netpeak.net>
2024-06-19 11:50:01 +0200

    fix: changed pods source (branch => 'main') chore: version 2.3.3

--- Sources/AttributionServerIntegration/Workers/AttributionDataWorker.swift
+++ Sources/AttributionServerIntegration/Workers/AttributionDataWorker.swift
@@ -38,7 +38,7 @@ class AttributionDataWorker: AttributionDataWorkerProtocol {
     }
     
     var sdkVersion: String {
-        return "2.3.2"
+        return "2.3.3"
     }
     
     var osVersion: String {
