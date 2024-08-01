a3e4e5d4d8e19fde77a6dfb1b05d492589f1cee1
Texas <a.kanarskyi@netpeak.net>
2024-08-01 16:09:25 +0200

    feat: updated Attribution install request model

--- Sources/AttributionServerIntegration/Models/InternalModels/AttributionInstallRequestModel.swift
+++ Sources/AttributionServerIntegration/Models/InternalModels/AttributionInstallRequestModel.swift
@@ -47,21 +47,51 @@ internal struct AttributionInstallRequestModel: Codable {
         let creativeSetName: String
         let token: String
         
+        let countryOrRegion: String
+        let adId: String
+        
+//        init(data:[String:Any]) {
+//            attribution = data["iad-attribution"] as? Bool ?? false
+//            orgName = data["iad-org-name"] as? String ?? ""
+//            orgId = data["iad-org-id"] as? String ?? ""
+//            campaignName = data["iad-campaign-name"] as? String ?? ""
+//            campaignId = data["iad-campaign-id"] as? String ?? ""
+//            purchaseDate = data["iad-purchase-date"] as? String ?? ""
+//            conversionDate = data["iad-conversion-date"] as? String ?? ""
+//            conversionType = data["iad-conversion-type"] as? String ?? ""
+//            clickDate = data["iad-click-date"] as? String ?? ""
+//            adGroupId = data["iad-adgroup-id"] as? String ?? ""
+//            adGroupName = data["iad-adgroup-name"] as? String ?? ""
+//            region = data["iad-country-or-region"] as? String ?? ""
+//            keyword = data["iad-keyword"] as? String ?? ""
+//            keywordId = data["iad-keyword-id"] as? String ?? ""
+//            keywordMatchType = data["iad-keyword-matchtype"] as? String ?? ""
+//            creativeSetId = data["iad-creativeset-id"] as? String ?? ""
+//            creativeSetName = data["iad-creativeset-name"] as? String ?? ""
+//            token = data["token"] as? String ?? ""
+//        }
+        
+        /*
+         attributionDetailsJson ["orgId": 1234567890, "adGroupId": 1234567890, "conversionType": Download, "keywordId": 12323222, "countryOrRegion": US, "adId": 1234567890, "campaignId": 1234567890, "attribution": 1]
+         */
         init(data:[String:Any]) {
-            attribution = data["iad-attribution"] as? Bool ?? false
+            attribution = data["attribution"] as? Bool ?? false
+            orgId = data["orgId"] as? String ?? ""
+            campaignId = data["campaignId"] as? String ?? ""
+            conversionType = data["conversionType"] as? String ?? ""
+            adGroupId = data["adGroupId"] as? String ?? ""
+            keywordId = data["keywordId"] as? String ?? ""
+            countryOrRegion = data["countryOrRegion"] as? String ?? ""
+            adId = data["adId"] as? String ?? ""
+            ///////
             orgName = data["iad-org-name"] as? String ?? ""
-            orgId = data["iad-org-id"] as? String ?? ""
             campaignName = data["iad-campaign-name"] as? String ?? ""
-            campaignId = data["iad-campaign-id"] as? String ?? ""
             purchaseDate = data["iad-purchase-date"] as? String ?? ""
             conversionDate = data["iad-conversion-date"] as? String ?? ""
-            conversionType = data["iad-conversion-type"] as? String ?? ""
             clickDate = data["iad-click-date"] as? String ?? ""
-            adGroupId = data["iad-adgroup-id"] as? String ?? ""
             adGroupName = data["iad-adgroup-name"] as? String ?? ""
             region = data["iad-country-or-region"] as? String ?? ""
             keyword = data["iad-keyword"] as? String ?? ""
-            keywordId = data["iad-keyword-id"] as? String ?? ""
             keywordMatchType = data["iad-keyword-matchtype"] as? String ?? ""
             creativeSetId = data["iad-creativeset-id"] as? String ?? ""
             creativeSetName = data["iad-creativeset-name"] as? String ?? ""
@@ -86,6 +116,8 @@ internal struct AttributionInstallRequestModel: Codable {
             keywordMatchType = ""
             creativeSetId = ""
             creativeSetName = ""
+            countryOrRegion = ""
+            adId = ""
             self.token = token
         }
     }
