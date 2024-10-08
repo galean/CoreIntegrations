09dd9877882191471d95672f0617831a7f8da289
Texas <a.kanarskyi@netpeak.net>
2024-10-08 13:01:09 +0200

    feat: Facebook sdk updated to 17.3.0

--- Sources/AttributionServerIntegration/Workers/AttributionDataWorker.swift
+++ Sources/AttributionServerIntegration/Workers/AttributionDataWorker.swift
@@ -30,7 +30,7 @@ class AttributionDataWorker: AttributionDataWorkerProtocol {
     }
     
     var sdkVersion: String {
-        return "2.4.5"
+        return "2.4.6"
     }
     
     var osVersion: String {
