411ad0d324a2b7e0bad489c64f3d50c66bb65135
galean <galean.pal@gmail.com>
2024-12-17 18:51:08 +0200

    chore: removed unused code

--- Sources/PurchasesIntegration/PurchaseManager/PurchasesManager.swift
+++ Sources/PurchasesIntegration/PurchaseManager/PurchasesManager.swift
@@ -61,24 +61,6 @@ public class PurchasesManager: NSObject, PurchasesManagerProtocol {
     public func setUserID(_ id: String) {
         self.userId = id
     }
-    
-    func topMostViewController(base: UIViewController? = UIApplication.shared.connectedScenes
-                                        .compactMap { ($0 as? UIWindowScene)?.keyWindow }
-                                        .first?.rootViewController) -> UIViewController? {
-        if let nav = base as? UINavigationController {
-            return topMostViewController(base: nav.visibleViewController)
-        }
-        
-        if let tab = base as? UITabBarController {
-            return topMostViewController(base: tab.selectedViewController)
-        }
-        
-        if let presented = base?.presentedViewController {
-            return topMostViewController(base: presented)
-        }
-        
-        return base
-    }
 }
 
 
