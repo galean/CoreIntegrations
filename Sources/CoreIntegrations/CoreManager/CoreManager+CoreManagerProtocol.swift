71312d2ea3a170f1d32eadd21fd8c8513eedf6ea
galean <galean.pal@gmail.com>
2024-12-17 12:21:49 +0200

    feat: Added availability check

--- Sources/CoreIntegrations/CoreManager/CoreManager+CoreManagerProtocol.swift
+++ Sources/CoreIntegrations/CoreManager/CoreManager+CoreManagerProtocol.swift
@@ -8,15 +8,18 @@ extension CoreManager: CoreManagerProtocol {
     @MainActor
     public func purchase(_ purchase: Purchase) async -> PurchasesPurchaseResult {
         var keyWindow: UIWindow? = nil
-        if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene {
-            keyWindow = windowScene.windows.first(where: { $0.isKeyWindow })
-            let purchaseWindow = UIWindow(windowScene: windowScene)
-            purchaseWindow.isHidden = true
-            purchaseWindow.windowLevel = .normal - 1
-            purchaseWindow.rootViewController = UIViewController()
-            purchaseWindow.makeKeyAndVisible()
-            
-            paywallWindow = purchaseWindow
+        
+        if #available (iOS 18.2, *) {
+            if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene {
+                keyWindow = windowScene.windows.first(where: { $0.isKeyWindow })
+                let purchaseWindow = UIWindow(windowScene: windowScene)
+                purchaseWindow.isHidden = true
+                purchaseWindow.windowLevel = .normal - 1
+                purchaseWindow.rootViewController = UIViewController()
+                purchaseWindow.makeKeyAndVisible()
+                
+                paywallWindow = purchaseWindow
+            }
         }
         
         
