2b1d42270191774b1fd755beee987a3a14e47352
galean <galean.pal@gmail.com>
2024-12-18 12:21:07 +0200

    fix: Optimal purchases fix for 18.2

--- Sources/PurchasesIntegration/Protocols/PublicProtocols/PurchasesManagerProtocol.swift
+++ Sources/PurchasesIntegration/Protocols/PublicProtocols/PurchasesManagerProtocol.swift
@@ -9,7 +9,7 @@ public protocol PurchasesManagerProtocol {
     func requestProducts(_ identifiers: [String]) async -> SKProductsResult
     func requestAllProducts(_ identifiers: [String]) async -> SKProductsResult
     func updateProductStatus() async
-    func purchase(_ product: Product) async throws -> SKPurchaseResult
+    func purchase(_ product: Product, activeController: UIViewController?) async throws -> SKPurchaseResult
 //    func purchase(_ product: Product, promoOffer:SKPromoOffer) async throws -> SKPurchaseResult
     func restore() async -> SKRestoreResult
     func restoreAll() async -> SKRestoreResult
