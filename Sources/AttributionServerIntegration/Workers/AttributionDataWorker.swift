c5d0af85992ad011ff7054041323d008bc89a3d3
galean <galean.pal@gmail.com>
2024-12-18 12:21:15 +0200

    chore: version update

--- Sources/AttributionServerIntegration/Workers/AttributionDataWorker.swift
+++ Sources/AttributionServerIntegration/Workers/AttributionDataWorker.swift
@@ -30,7 +30,7 @@ class AttributionDataWorker: AttributionDataWorkerProtocol {
     }
     
     var sdkVersion: String {
-        return "2.4.9"
+        return "2.4.11"
     }
     
     var osVersion: String {
