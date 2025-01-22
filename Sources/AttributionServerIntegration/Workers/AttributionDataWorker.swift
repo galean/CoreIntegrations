789db5337983b753d921c6c291bbbfadba59791b
Texas <a.kanarskyi@netpeak.net>
2025-01-22 13:35:31 +0100

    feat: merged main chore: version 2.4.15attest

--- Sources/AttributionServerIntegration/Workers/AttributionDataWorker.swift
+++ Sources/AttributionServerIntegration/Workers/AttributionDataWorker.swift
@@ -30,7 +30,7 @@ class AttributionDataWorker: AttributionDataWorkerProtocol {
     }
     
     var sdkVersion: String {
-        return "2.4.15"
+        return "2.4.15attest"
     }
     
     var osVersion: String {
