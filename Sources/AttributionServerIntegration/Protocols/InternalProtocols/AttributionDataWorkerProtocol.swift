8d1a8a7a72d2d905f45135973c2273937538e00d
Texas <a.kanarskyi@netpeak.net>
2025-01-23 10:50:54 +0100

    feat: improved attributionToken retrieve

--- Sources/AttributionServerIntegration/Protocols/InternalProtocols/AttributionDataWorkerProtocol.swift
+++ Sources/AttributionServerIntegration/Protocols/InternalProtocols/AttributionDataWorkerProtocol.swift
@@ -9,10 +9,15 @@ internal protocol AttributionDataWorkerProtocol {
     var osVersion: String { get }
     var appVersion: String { get }
     var isAdTrackingEnabled: Bool { get }
-    func attributionDetails() async throws -> [String: Any]?
+    func attributionDetails() async -> AttributionDetails?
     var storeCountry: String { get }
     
     var receiptToken: String { get }
     
     func generateUniqueToken() -> String
+}
+
+struct AttributionDetails {
+    var details:[String: Any]?
+    var attributionToken: String
 }
