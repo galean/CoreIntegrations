719c0907c0937cb68def63d256080e75fcf146df
galean <galean.pal@gmail.com>
2025-02-20 15:36:49 +0200

    feat: Added possibility to test ASA with onelink

--- Sources/AttributionServerIntegration/Workers/AttributionDataWorker.swift
+++ Sources/AttributionServerIntegration/Workers/AttributionDataWorker.swift
@@ -30,7 +30,7 @@ class AttributionDataWorker: AttributionDataWorkerProtocol {
     }
     
     var sdkVersion: String {
-        return "2.4.19"
+        return "2.4.20"
     }
     
     var osVersion: String {
